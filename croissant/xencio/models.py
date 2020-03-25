fd33784ce062934428f8e20516f82a9cbbe23502
Yang MO <ymo@wpi.edu>
2020-03-25 19:40:24 +0800

    creat xencio query model

--- croissant/xencio/models.py
+++ croissant/xencio/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Query(models.Model):
     account = models.CharField(max_length=80)
-    search_data = models.DateField()
-    raw_result = models.CharField(max_length=200)
+    query_time = models.DateField()
+    meta_data = models.CharField(max_length=200)
     bank = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
     currency = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.bank + ": "+ self.account
+        return self.bank + ": " + self.account
